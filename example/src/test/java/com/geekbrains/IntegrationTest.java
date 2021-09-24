f4c57bde704ba0227d78b5b25513ce5bed45776b
alexbohdanov <83947461+alexbohdanov@users.noreply.github.com>
2021-09-24 21:59:39 +1000

    Add files via upload

---
+++ example/src/test/java/com/geekbrains/IntegrationTest.java
@@ -0,0 +1,18 @@
+package com.geekbrains;
+
+import org.junit.Test;
+
+public class IntegrationTest {
+
+
+
+    @Test
+    public void shouldAnswerWithTrue()  {
+        System.out.println("Integration test1");
+    }
+
+    @Test
+    public void assertTrue2() {
+        System.out.println("Integration test2");
+    }
+}
